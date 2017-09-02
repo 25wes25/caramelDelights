83d2898103067a165e3dc9a3098fcfe8734f94d4
mulvaneya7 <mulvaneya7@gmail.com>
2017-09-02 12:07:27 -0700

    Add files via upload

---
+++ CollegeTour 2.0/CollegeTour2/main.cpp
@@ -0,0 +1,14 @@
+#include "mainwindow.h"
+#include "login.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    login log;
+
+    log.show();
+
+    return a.exec();
+}
